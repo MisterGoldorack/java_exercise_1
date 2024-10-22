f6bc4bf81b65c8b301a95b79f0f7e4809ea3c5e2
MisterGoldorack <damien.dassonville@epita.fr>
2024-10-22 16:42:01 +0200

    Add Fibo Command

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the First Exercise");
+
+        for (;;) {
+            var scanner = new Scanner(System.in);
+
+            if (scanner.next().equals("quit")) {
+                System.exit(0);
+            } else if (scanner.next().equals("fibo")) {
+                System.out.println("Choose a n value !");
+                int n = scanner.nextInt();
+                System.out.println("Fibo(" + n + ") = " + Fibo.fibo(n));
+            } else {
+                System.out.println("Unknow Command");
+            }
+        }
+    }
 }
